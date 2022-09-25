bbc54371dd4bcd16ddc49afdeaa49d27ad56be0b
hogejiro <1515179+hogejiro@users.noreply.github.com>
2022-09-25 13:59:22 +0900

    press CTRL-Q to quit

--- build_your_own_text_editor/kilo.c
+++ build_your_own_text_editor/kilo.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 struct termios orig_termios;
 
@@ -42,7 +43,7 @@ int main() {
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
-        if (c == 'q') break;
+        if (c == CTRL_KEY('q')) break;
     }
     return 0;
 }
