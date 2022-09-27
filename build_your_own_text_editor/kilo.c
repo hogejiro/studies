6512a7807a0e97e771164251720ea25052a4f988
hogejiro <1515179+hogejiro@users.noreply.github.com>
2022-09-28 00:44:20 +0900

    The Home and End keys

--- build_your_own_text_editor/kilo.c
+++ build_your_own_text_editor/kilo.c
@@ -16,6 +16,8 @@ enum editorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN
 };
@@ -57,6 +59,8 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+// The Home key could be sent as <esc>[1~, <esc>[7~, <esc>[H, or <esc>OH
+// The End  key could be sent as <esc>[4~, <esc>[8~, <esc>[F, or <esc>OF
 int editorReadKey() {
     int nread;
     char c;
@@ -75,6 +79,12 @@ int editorReadKey() {
                 if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
                 if (seq[2] == '~') {
                     switch (seq[1]) {
+                        case '1':
+                        case '7':
+                            return HOME_KEY;
+                        case '4':
+                        case '8':
+                            return END_KEY;
                         case '5':
                             return PAGE_UP;
                         case '6':
@@ -91,7 +101,18 @@ int editorReadKey() {
                         return ARROW_RIGHT;
                     case 'D':
                         return ARROW_LEFT;
+                    case 'H':
+                        return HOME_KEY;
+                    case 'F':
+                        return END_KEY;
                 }
+            }
+        } else if (seq[0] == 0) {
+            switch (seq[1]) {
+                case 'H':
+                    return HOME_KEY;
+                case 'F':
+                    return END_KEY;
             }
         }
         return '\x1b';
@@ -231,6 +252,12 @@ void editorProcessKeyPress() {
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols - 1;
             break;
         case PAGE_UP:
         case PAGE_DOWN: {
